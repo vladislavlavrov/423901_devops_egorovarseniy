76ba23775c062e206203917fa798bb48387beb8a
VladislavLavrov <lavrov.vladislav@gmail.com>
2025-11-17 10:54:43 +0500

    1.25.11.01
    Перенес проект практических работ в папку App_practical

---
+++ App_practical/Data/CalculatorContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace _4_Calculator.Data
+{
+    public class CalculatorContext: DbContext
+    {
+        public DbSet<DataImputVariant> DataImputVariants { get; set; }
+
+        public CalculatorContext(DbContextOptions<CalculatorContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //OnModelCreating(modelBuider);
+        }
+    }
+}
