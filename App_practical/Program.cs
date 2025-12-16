34a37b54a10e585f039f0e17c43372039129a798
Itilich <aaperlovka@gmail.com>
2025-12-16 11:10:58 +0500

    adfqdqw

--- App_practical/Program.cs
+++ App_practical/Program.cs
@@ -17,12 +17,12 @@ namespace Calculator
 
                 meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
 
-                meterProviderBuilder.AddView("http.server.request.duration", 
+                meterProviderBuilder.AddView("http.server.request.duration",
                     new ExplicitBucketHistogramConfiguration
-                    { 
+                    {
                         Boundaries = new double[]
-                        { 
-                            0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.25, 0.75, 1, 2.5, 5, 7.5, 10
+                        {
+                            0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
                         }
                     });
             });
