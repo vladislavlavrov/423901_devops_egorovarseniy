4e8626fa73a47042cc330e392b639fcd673f8466
Itilich <aaperlovka@gmail.com>
2025-12-16 11:04:17 +0500

    ewfrvwew

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using OpenTelemetry.Metrics;
+
 namespace Calculator
 {
     public class Program
@@ -5,6 +7,24 @@ namespace Calculator
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddOpenTelemetry().WithMetrics(meterProviderBuilder =>
+            {
+                meterProviderBuilder.AddPrometheusExporter();
+
+                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+                                      "Microsoft.AspNetCore.Server.Kestrel");
+
+                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+                meterProviderBuilder.AddView("http.server.request.duration",
+                    new ExplicitBucketHistogramConfiguration
+                    {
+                        Boundaries = new double[]
+                        {
+                            0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.25, 0.75, 1, 2.5, 5, 7.5, 10
+                        }
+                    });
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -33,7 +53,7 @@ namespace Calculator
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
+            app.MapPrometheusScrapingEndpoint();
             app.Run();
         }
     }
