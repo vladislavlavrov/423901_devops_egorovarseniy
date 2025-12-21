f5924e4123e80a89b550fb1a37e0034c641aabc2
Itilich <aaperlovka@gmail.com>
2025-12-21 12:57:30 +0500

    Начало работы

--- App_practical/Controllers/HomeController.cs
+++ App_practical/Controllers/HomeController.cs
@@ -17,48 +17,6 @@ namespace Calculator.Controllers
         {
             return View();
         }
-        [HttpGet]
-        public IActionResult Calculator()
-        {
-            return View(new CalculatorModel()); 
-        }
-        [HttpPost]
-        public IActionResult Calculator(double Number1, double Number2, string Operation)
-        {
-            var model = new CalculatorModel
-            {
-                Number1 = Number1,
-                Number2 = Number2,
-                Operation = Operation
-            };
-
-            _logger.LogInformation($"Received: {Number1} {Operation} {Number2}");
-
-            try
-            {
-                model.Result = Operation switch
-                {
-                    "+" => Number1 + Number2,
-                    "-" => Number1 - Number2,
-                    "*" => Number1 * Number2,
-                    "/" when Number2 != 0 => Number1 / Number2,
-                    "/" when Number2 == 0 => throw new DivideByZeroException(),
-                    _ => throw new ArgumentException("Неверная операция")
-                };
-
-                _logger.LogInformation($"Result: {model.Result}");
-            }
-            catch (DivideByZeroException)
-            {
-                model.ErrorMessage = "Ошибка: деление на ноль!";
-            }
-            catch (Exception ex)
-            {
-                model.ErrorMessage = $"Ошибка: {ex.Message}";
-            }
-
-            return View(model);
-        }
         public IActionResult Privacy()
         {
             return View();
